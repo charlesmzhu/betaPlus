e8a29d968fd51c15c7edfb265c1f748ae3e11809
Charles Zhu <charlesmzhu@gmail.com>
2015-05-20 00:45:06 -0700

    Some exploratory data analysis and initial functions

--- masterRScript.R
+++ masterRScript.R
@@ -1,1 +1,22 @@
-stockList <- [1,2,3,4,5]
+
+stockList <- read.table("constituents.csv", header = TRUE, sep=",")[,1]
+
+getUrl <- function (symbol) {
+	url <- paste("http://api.kibot.com/?action=history&symbol=", symbol, "&interval=daily&period=5000", sep = "")
+	return(url)
+}
+
+downloadData <- function ( listOfStocks ) { 
+	#This is the master function that downloads all relevant data and applies transformations
+}
+
+percentChange <- function ( list ) {
+	return ( ( list[, 5] - list[, 2] ) / list [, 2] * 100 )
+}
+
+########################### SANDBOX ##############################
+
+SPX <- read.csv ( getUrl ( "$SPX") )
+table <- read.csv ( getUrl ( toString(stockList[1]) ) )
+testSet <- percentChange ( SPX )
+sum ( testSet > 0 )
